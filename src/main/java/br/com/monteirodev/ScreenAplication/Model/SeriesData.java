e315e7c82df95291a6942097df4a12f90d9013fd
monteirobiel <gabriel.m.oliveira03@gmail.com>
2024-06-01 02:38:11 +0100

    Feat: add new SeriesData Record

---
+++ src/main/java/br/com/monteirodev/ScreenAplication/Model/SeriesData.java
@@ -0,0 +1,6 @@
+package br.com.monteirodev.ScreenAplication.Model;
+
+public record SeriesData (String Title,
+                          int totalSeasons,
+                          String Ratings){
+}
