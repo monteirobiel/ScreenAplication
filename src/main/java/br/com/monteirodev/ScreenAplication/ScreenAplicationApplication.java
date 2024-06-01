fecde0b3f5c81ee068225d99a3a34793eed685d0
monteirobiel <gabriel.m.oliveira03@gmail.com>
2024-06-01 03:22:01 +0100

    Feat: add API Consumption and data conversion

--- src/main/java/br/com/monteirodev/ScreenAplication/ScreenAplicationApplication.java
+++ src/main/java/br/com/monteirodev/ScreenAplication/ScreenAplicationApplication.java
@@ -1,13 +1,29 @@
 package br.com.monteirodev.ScreenAplication;
 
+import br.com.monteirodev.ScreenAplication.Model.SeriesData;
+import br.com.monteirodev.ScreenAplication.Service.ConsumptionAPI;
+import br.com.monteirodev.ScreenAplication.Service.DataConvert;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenAplicationApplication {
+public class ScreenAplicationApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenAplicationApplication.class, args);
+
 	}
 
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		var  consumptionAPI = new ConsumptionAPI();
+		var json = consumptionAPI.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=9b4ee23");
+		System.out.println(json);
+		DataConvert convert = new DataConvert();
+		SeriesData data = convert.getData (json, SeriesData.class);
+		System.out.println(data);
+	}
 }
