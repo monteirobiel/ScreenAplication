a105363f3d12bc0d9ed83121eb2df81efa6407c4
monteirobiel <gabriel.m.oliveira03@gmail.com>
2024-06-01 01:24:09 +0100

    Initial commit

---
+++ src/main/java/br/com/monteirodev/ScreenAplication/ScreenAplicationApplication.java
@@ -0,0 +1,13 @@
+package br.com.monteirodev.ScreenAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/monteirodev/ScreenAplication/ScreenAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.monteirodev.ScreenAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
