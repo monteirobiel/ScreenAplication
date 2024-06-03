19c1664c1fcae17d2883e93a01f1943e6cd4f632
monteirobiel <gabriel.m.oliveira03@gmail.com>
2024-06-03 21:56:59 +0100

    refactor: rename "Rated" for "imbRating"

--- src/main/java/br/com/monteirodev/ScreenAplication/Model/EpisodeData.java
+++ src/main/java/br/com/monteirodev/ScreenAplication/Model/EpisodeData.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record EpisodeData(String Title,
                           Integer Episode,
-                          String Rated,
+                          String imdbRating,
                           String Released) {
 }
