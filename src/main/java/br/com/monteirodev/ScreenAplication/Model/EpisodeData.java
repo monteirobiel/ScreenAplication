9d936cbdd1da1a0dfd41d9778deffc6e1a6cdb1d
monteirobiel <gabriel.m.oliveira03@gmail.com>
2024-06-03 21:17:12 +0100

    Feat: add Record for represents Data

---
+++ src/main/java/br/com/monteirodev/ScreenAplication/Model/EpisodeData.java
@@ -0,0 +1,7 @@
+package br.com.monteirodev.ScreenAplication.Model;
+
+public record EpisodeData(String Title,
+                          Integer Episode,
+                          String Rated,
+                          String Released) {
+}
