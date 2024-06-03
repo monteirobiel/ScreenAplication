f55deefa124d1a832873451ef6aea2c8572e6e8a
monteirobiel <gabriel.m.oliveira03@gmail.com>
2024-06-03 21:37:45 +0100

    Feat: add Jackson Annotation for Json Ignore

--- src/main/java/br/com/monteirodev/ScreenAplication/Model/EpisodeData.java
+++ src/main/java/br/com/monteirodev/ScreenAplication/Model/EpisodeData.java
@@ -1,5 +1,8 @@
 package br.com.monteirodev.ScreenAplication.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record EpisodeData(String Title,
                           Integer Episode,
                           String Rated,
